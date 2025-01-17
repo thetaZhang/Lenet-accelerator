b29e3832913bb5a42d2527ee1d221c456d28d57e
zhy <Zhy_theta@outlook.com>
2025-01-17 22:31:53 +0800

    fix

--- src/Dffs.v
+++ src/Dffs.v
@@ -3,22 +3,20 @@
 
 // Dff with asynchronous negative edge reset
 module DffNegRst #(
-  parameter DATA_WIDTH = 1
-)(
-  input clk,
-  input rst_n
+    parameter DATA_WIDTH = 1
+) (
+    input clk,
+    input rst_n,
 
-  input [DATA_WIDTH - 1 : 0] d,
-  output [DATA_WIDTH - 1 : 0] q
+    input  [DATA_WIDTH - 1 : 0] d,
+    output [DATA_WIDTH - 1 : 0] q
 );
 
   reg [DATA_WIDTH - 1 : 0] q_reg;
 
   always @(posedge clk or negedge rst_n) begin
-    if (~rst_n) 
-      q_reg <= {DATA_WIDTH{1'b0}};
-    else 
-      q_reg <= d;
+    if (~rst_n) q_reg <= {DATA_WIDTH{1'b0}};
+    else q_reg <= d;
   end
 
   assign q = q_reg;
@@ -27,22 +25,21 @@ endmodule
 
 // Dff with asynchronous positive edge reset
 module DffPosRst #(
-  parameter DATA_WIDTH = 1
-)(
-  input clk,
-  input rst
+    parameter DATA_WIDTH = 1
+) (
+    input clk,
+    input rst,
 
-  input [DATA_WIDTH - 1 : 0] d,
-  output [DATA_WIDTH - 1 : 0] q
+    input  [DATA_WIDTH - 1 : 0] d,
+    output [DATA_WIDTH - 1 : 0] q
 );
 
   reg [DATA_WIDTH - 1 : 0] q_reg;
 
   always @(posedge clk or posedge rst) begin
-    if (rst) 
-      q_reg <= {DATA_WIDTH{1'b0}};
-    else 
-      q_reg <= d;
+    if (rst) q_reg <= {DATA_WIDTH{1'b0}};
+    else q_reg <= d;
   end
 
   assign q = q_reg;
+endmodule
